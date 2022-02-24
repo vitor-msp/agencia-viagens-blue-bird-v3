129a7d307a7ce002980222474c9ea3393565bea2
Vitor Mateus <vitor7jan@gmail.com>
2022-02-24 20:51:43 -0300

    config security models

--- backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/models/repositories/UserRepository.java
+++ backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/models/repositories/UserRepository.java
@@ -1,5 +1,14 @@
 package br.com.agenciaviagens.bluebird.models.repositories;
 
-public class UserRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import br.com.agenciaviagens.bluebird.models.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+	Boolean existsByUsername(String username);
+	Boolean existsByEmail(String email);
 }
