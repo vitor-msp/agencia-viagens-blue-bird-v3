f38ec8b99b147c06755cdaff5a5c631b26740172
Vitor Mateus <vitor7jan@gmail.com>
2022-02-24 21:43:55 -0300

    config payload

--- backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/payload/request/LoginRequest.java
+++ backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/payload/request/LoginRequest.java
@@ -1,5 +1,27 @@
 package br.com.agenciaviagens.bluebird.payload.request;
 
-public class LoginRequest {
+import javax.validation.constraints.NotBlank;
 
+public class LoginRequest {
+	@NotBlank
+	private String username;
+
+	@NotBlank
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/payload/response/MessageResponse.java
+++ backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/payload/response/MessageResponse.java
@@ -1,5 +1,17 @@
 package br.com.agenciaviagens.bluebird.payload.response;
 
 public class MessageResponse {
+  private String message;
 
+  public MessageResponse(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
