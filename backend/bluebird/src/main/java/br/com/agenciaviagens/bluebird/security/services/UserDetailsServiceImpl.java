b4b8e324e51eca2f1d71055f2808a3bdb90de4d3
Vitor Mateus <vitor7jan@gmail.com>
2022-02-24 21:39:14 -0300

    config security layer

--- backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/security/services/UserDetailsServiceImpl.java
+++ backend/bluebird/src/main/java/br/com/agenciaviagens/bluebird/security/services/UserDetailsServiceImpl.java
@@ -1,5 +1,24 @@
 package br.com.agenciaviagens.bluebird.security.services;
 
-public class UserDetailsServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.agenciaviagens.bluebird.models.entities.User;
+import br.com.agenciaviagens.bluebird.models.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	UserRepository userRepository;
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+		return UserDetailsImpl.build(user);
+	}
 }
